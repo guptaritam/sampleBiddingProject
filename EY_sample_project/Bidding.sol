f4be358f27f39089f360c0d6afed8085f6f2bdc2
Ritam Gupta <ritamg123@gmail.com>
2018-11-27 22:17:58 +0530

    Update Bidding.sol

--- EY_sample_project/Bidding.sol
+++ EY_sample_project/Bidding.sol
@@ -1,22 +1,25 @@
 pragma solidity ^ 0.4 .17;
 
+//This contract is for registration of a single item for bidding
 contract Bidding {
 
     address admin;
     string itemName;
     uint itemBasePrice;
 
+//This structure would be used to save bids from party B and C. For now only two attributes are kept for demo purposes
     struct bid {
-        address biddingAddress;
+        string itemName;
         uint bidAmount;
     }
 
-    mapping(string => bid) bids;
+//Indexed Mapping to keep a track and easy retrieval of bids. #Key# here is unique bidderAddress of party B and C
+    mapping(address => bid) bids;
 
-    event registerItemEvent(string _actionPerformed, string _itemName, uint _itemBasePrice);
+    event registerItemEvent(string _actionPerformed, string _itemName, uint _itemBasePrice, uin256 _timeOfTx);
 
-    modifier onlyNodeA(address _address) {
-        if (_address == admin)
+    modifier onlyNodeA() {
+        if (msg.sender != admin)
         throw;
         _;
     }
@@ -25,30 +28,18 @@ contract Bidding {
         admin = msg.sender;
     }
 
-    function registerItem(address _address, string _itemName, uint _itemBasePrice) onlyNodeA(_address) {
-
-        //can apply validators as below 
-        //require(isItemRegistered[_itemName] == false);
-        
+    function registerItem(string _itemName, uint _itemBasePrice) onlyNodeA() {
         itemName = _itemName;
         itemBasePrice = _itemBasePrice;
-    
-        //isItemRegistered[_itemName] = true;
-
-        registerItemEvent("ITEM REGISTERED", _itemName, _itemBasePrice);
+        registerItemEvent("ITEM REGISTERED", _itemName, _itemBasePrice, now);
     }
 
-    //considering only a single item 
     function viewItem(string _itemName) returns(uint) {
-
-        //require(isItemRegistered[_itemName] == true);
-
-        return (itemBasePrice);
+       return (itemBasePrice);
     }
 
-    function placeBid(string _itemName, address _biddingAddress, uint _bidAmount) {
-
-        bids[_itemName].biddingAddress = _biddingAddress;
-        bids[_itemName].bidAmount = _bidAmount; 
+    function placeBid(string _itemName, address _bidderAddress, uint _bidAmount) {
+        bids[_bidderAddress].itemName = _itemName;
+        bids[_bidderAddress].bidAmount = _bidAmount; 
     }
 }
